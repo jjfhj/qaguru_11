30759e5f1b84757cd666c8c2a83321ed4a2a060a
Karina <aiwpront@gmail.com>
2021-11-30 00:13:56 +0400

    update tags

--- src/test/java/com/github/jjfhj/tests/OzonTest.java
+++ src/test/java/com/github/jjfhj/tests/OzonTest.java
@@ -2,12 +2,11 @@ package com.github.jjfhj.tests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.github.jjfhj.tests.TestData.OZON_URL;
 
@@ -15,29 +14,11 @@ public class OzonTest extends TestBase {
 
     @ValueSource(strings = {"Всё о Муми-троллях. Книга 1", "Всё о Муми-троллях. Книга 2"})
     @DisplayName("Результаты поиска")
-    @Tag("Blocker")
-    @Tag("High")
-    @Tag("Web")
+    @Tags({@Tag("Blocker"), @Tag("High"), @Tag("Web")})
     @ParameterizedTest(name = "Отображение товара {0} в результатах поиска")
     void checkingBookInSearchResults(String searchQuery) {
         open(OZON_URL);
         $("[name='text']").setValue("Всё о Муми-троллях").submit();
         $$("[class='b3a6 a3m3']").shouldHave(texts(searchQuery));
-    }
-
-    @CsvSource({
-            "Мягкая обложка, Обложка: Мягкая обложка",
-            "Твердый переплет, Обложка: Твердый переплет"
-    })
-    @DisplayName("Теги фильтра 'Обложка'")
-    @Tag("Minor")
-    @Tag("Low")
-    @Tag("Web")
-    @ParameterizedTest(name = "Отображение тега {0}")
-    void checkingDisplayOfCoverFilterTag(String filterСheckbox, String tagName) {
-        open(OZON_URL);
-        $("[name='text']").setValue("Вторая жизнь Уве").submit();
-        $$(".ui-D6").findBy(text(filterСheckbox)).click();
-        $("[class='ui-l5 ui-l6']").shouldHave(text(tagName));
     }
 }
