90e558dc19413b4fe395737d03635dc5eddbf4e2
Karina <aiwpront@gmail.com>
2021-11-29 14:18:43 +0400

    update OzonTest

--- src/test/java/com/github/jjfhj/tests/OzonTest.java
+++ src/test/java/com/github/jjfhj/tests/OzonTest.java
@@ -4,16 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.github.jjfhj.TestData.OZON_URL;
 import static com.github.jjfhj.tests.TestData.OZON_URL;
 
 public class OzonTest extends TestBase {
@@ -44,30 +39,5 @@ public class OzonTest extends TestBase {
         $("[name='text']").setValue("Вторая жизнь Уве").submit();
         $$(".ui-D6").findBy(text(filterСheckbox)).click();
         $("[class='ui-l5 ui-l6']").shouldHave(text(tagName));
-    }
-
-    @EnumSource(ProfileMenu.class)
-    @DisplayName("Пункты меню неавторизованного пользователя")
-    @Tag("Blocker")
-    @Tag("High")
-    @Tag("Web")
-    @ParameterizedTest(name = "Отображение пункта меню {0}")
-    void checkingDisplayOfAnonymousMenuItem(ProfileMenu profileMenu) {
-        open(OZON_URL);
-        $(".g5j6").shouldHave(text(profileMenu.getProfileMenu()));
-    }
-
-    @MethodSource("com.github.jjfhj.CatalogItems#productCategories")
-    @DisplayName("Подкатегории на страницах товарных категорий")
-    @Tag("Minor")
-    @Tag("Low")
-    @Tag("Web")
-    @ParameterizedTest(name = "Отображение подкатегорий на странице товарной категории {0}")
-    void checkingProductCategoriesInCatalog(String catalogItems, List<String> goodsCategories) {
-        open(OZON_URL);
-        refresh(); // Для закрытия модального окна "Выбирайте лучшее!", альтернативными методами не получилось решить
-        $$(".ui-e6").findBy(text("Каталог")).click();
-        $$(".g5r1").findBy(text(catalogItems)).$(".g5s7").click();
-        $$(".cw2").shouldHave(texts(goodsCategories));
     }
 }
